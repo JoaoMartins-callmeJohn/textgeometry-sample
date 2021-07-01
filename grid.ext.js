62cd5a1b4026257ee2a2f46defe3f19a166dea96
JoaoMartins-Forge <75683555+JoaoMartins-Forge@users.noreply.github.com>
2021-07-01 19:19:36 -0300

    separating grid extension

---
+++ grid.ext.js
@@ -0,0 +1,26 @@
+class GridExtension extends Autodesk.Viewing.Extension {
+  constructor(viewer, options) {
+    super(viewer, options);
+    this.grid = null;
+  }
+
+  load() {
+    // add grid
+    this.grid = new THREE.GridHelper(600, 40);
+    this.grid.material.opacity = 0.8;
+    this.grid.material.transparent = true;
+    this.grid.rotation.x = 3.14/2.;
+    this.grid.position.set (0, 0, -40);
+    if (!this.viewer.overlays.hasScene('grid')) {
+        this.viewer.overlays.addScene('grid');
+    }
+    this.viewer.overlays.addMesh(this.grid, 'grid');
+    return true;
+  }
+
+  unload() {
+    return true;
+  }
+}
+
+Autodesk.Viewing.theExtensionManager.registerExtension('GridExtension', GridExtension);
